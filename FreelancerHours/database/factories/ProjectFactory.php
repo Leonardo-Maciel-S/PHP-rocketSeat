47de3c829be05f6b49122a61dc3c4a699a55b712
Leonardo Maciel <leonardomacielj@gmail.com>
2024-10-09 19:54:15 -0300

    started project, config model of user and project

---
+++ FreelancerHours/database/factories/ProjectFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use phpDocumentor\Reflection\PseudoTypes\HtmlEscapedString;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+
+
+    /**
+     * Define the model's default state.
+     *             'description' => htmlspecialchars(fake()->randomHtml()),
+
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+
+        return [
+            'title' => collect(fake()->words(5))->join(' '),
+            'description' => fake()->randomHtml(),
+            'ends_at' => fake()->dateTimeBetween('now', '+3 days'),
+            'status' => fake()->randomElement(['open', 'close']),
+            'tech_stacks' => fake()->randomElements(['react', 'php', 'laravel', 'vue', 'tailwind', 'javascript', 'nextjs', 'python'], random_int(1, 5)),
+            'created_by' => User::factory()
+        ];
+    }
+}
